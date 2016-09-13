b955433371a1625e978953b78cb81e3a95bf038e
Andrew <atandiawan@gmail.com>
2016-09-13 15:03:00 +0700

    Part 1

--- address_book.js
+++ address_book.js
@@ -1,1 +1,80 @@
+"use strict"
+const repl = require('repl'); // optional
+const sqlite = require('sqlite3').verbose();
 
+let file = 'address_book.db'
+let db = new sqlite.Database(file)
+
+class Contacts {
+  static addContact(firstname, lastname, phone, email, address) {
+    let ADD_STUDENT = `INSERT INTO contacts (firstname, lastname, phone, email, address) VALUES ('${firstname}', '${lastname}', '${phone}', '${email}', '${address}')`
+    Contacts.runDbCommand(ADD_STUDENT)
+  }
+
+  static deleteContact(id) {
+    let DELETE_STUDENT = `DELETE FROM student WHERE id = ${id}`
+    Contacts.runDbCommand(DELETE_STUDENT)
+  }
+
+  static displayTable(table_name) {
+    let DISPLAY_TABLE = `SELECT * FROM ${table_name}`
+    Contacts.runDbAllCommand(DISPLAY_TABLE)
+  }
+
+  static displayContactByName(firstname, lastname) {
+    let DISPLAY_STUDENT_BY_NAME = `SELECT * FROM student WHERE firstname LIKE '${firstname}' AND lastname LIKE '${lastname}'`
+    Contacts.runDbAllCommand(DISPLAY_STUDENT_BY_NAME)
+  }
+
+  static editAddress(id, newAddress){
+    let EDIT_ADDRESS = `UPDATE student SET address = '${newAddress}' WHERE id = '${id}'`
+    Contacts.runDbAllCommand(EDIT_ADDRESS)
+  }
+
+  static addGroup(groupName){
+    let ADD_GROUP = `INSERT INTO groups (groupname) VALUES ('${groupName}')`
+    Contacts.runDbCommand(ADD_GROUP)
+  }
+
+  static removeGroup(groupName){
+    let REMOVE_GROUP = `DELETE FROM groups WHERE groupname = '${groupName}'`
+    let REMOVE_GROUP_MEMBERS = `DELETE FROM group_members WHERE groupname = '${groupName}'`
+    Contacts.runDbCommand(REMOVE_GROUP)
+    Contacts.runDbCommand(REMOVE_GROUP_MEMBERS)
+  }
+
+  static addToGroup(groupName, member_id){
+    let ADD_TO_GROUP = `INSERT INTO group_members (groupname, member_id) VALUES ('${groupName}', '${member_id}')`
+    Contacts.runDbCommand(ADD_TO_GROUP)
+  }
+
+  static runDbCommand(command){
+    db.serialize(function() {
+      // Create table
+      db.run(command, function(err,data) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("Successful")
+          console.log(data)
+        }
+      })
+    })
+  }
+
+  static runDbAllCommand(command){
+    db.serialize(function() {
+      // Create table
+      db.all(command, function(err,data) {
+        if (err) {
+          console.log(err)
+        } else {
+          console.log(data)
+        }
+      })
+    })
+  }
+}
+
+let replServer = repl.start({prompt:'> '})
+replServer.context.Contacts = Contacts
--- setup.js
+++ setup.js
@@ -9,6 +9,8 @@ let db = new sqlite.Database(file)
 
 // SQL Statement
 let CREATE_TABLE = "CREATE TABLE IF NOT EXISTS contacts ( id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT NOT NULL, phone TEXT, email TEXT, address TEXT )"
+let CREATE_GROUP_TABLE = "CREATE TABLE IF NOT EXISTS groups ( id INTEGER PRIMARY KEY AUTOINCREMENT, groupname TEXT NOT NULL)"
+let CREATE_TRANSACTIONAL_TABLE = "CREATE TABLE IF NOT EXISTS group_members (id INTEGER PRIMARY KEY AUTOINCREMENT, groupname TEXT, member_id INTEGER, FOREIGN KEY(groupname) REFERENCES groups(groupname), FOREIGN KEY(member_id) REFERENCES contacts(id))"
 
 let address = JSON.parse(fs.readFileSync('address_book.json'))
 let SEED_DATA = "INSERT INTO contacts (firstname, lastname, phone, email) VALUES "
@@ -54,5 +56,43 @@ let seedData = () => {
   })
 }
 
+//CREATE GROUP TABLE
+let createGroupTable = () => {
+
+  // Run SQL one at a time
+  db.serialize(function() {
+
+    // Create table
+    db.run(CREATE_GROUP_TABLE, function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('CREATE GROUP TABLE')
+      }
+    })
+  })
+}
+
+//CREATE TRANSACTIONAL TABLE
+let createTransactionalTable = () => {
+
+  // Run SQL one at a time
+  db.serialize(function() {
+
+    // Create table
+    db.run(CREATE_TRANSACTIONAL_TABLE, function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('CREATE TRANSACTIONAL TABLE')
+      }
+    })
+  })
+}
+
+
+
 createTable()
 seedData()
+createGroupTable()
+createTransactionalTable()
