47bc5872e993dd18d8c8215f152fdf2e8383b7db
Peter Raswono <peter@mulmut.com>
2016-09-13 14:17:10 +0700

    Get data from JSON

--- setup.js
+++ setup.js
@@ -8,47 +8,51 @@ let file = 'address_book.db'
 let db = new sqlite.Database(file)
 
 // SQL Statement
-let CREATE_TABLE = "CREATE TABLE IF NOT EXISTS student ( id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT NOT NULL, phone TEXT, email TEXT )"
+let CREATE_TABLE = "CREATE TABLE IF NOT EXISTS contacts ( id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT NOT NULL, phone TEXT, email TEXT, address TEXT )"
 
 let address = JSON.parse(fs.readFileSync('address_book.json'))
+let SEED_DATA = "INSERT INTO contacts (firstname, lastname, phone, email) VALUES "
 
-for (let i = 0; i < address.length; i++)
+for (let i = 0; i < address.length; i++) {
+  SEED_DATA += "('" + address[i].firstname + "', '" + address[i].lastname + "', '" + address[i].phone + "', '" + address[i].email + "')"
+  if (address.length - 1 > i) {
+    SEED_DATA += ", "
+  }
+}
 
-// let SEED_DATA = "INSERT INTO student (firstname, lastname, birthdate) VALUES ('Peter', 'Raswono', '081324354657', 'peter@gmail.com'), ('Andrew', 'Tandiawan', '081234567890', 'andrew@gmail.com')"
+// CREATE_TABLE
+let createTable = () => {
 
-// // CREATE_TABLE
-// let createTable = () => {
+  // Run SQL one at a time
+  db.serialize(function() {
 
-//   // Run SQL one at a time
-//   db.serialize(function() {
+    // Create table
+    db.run(CREATE_TABLE, function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('CREATE TABLE')
+      }
+    })
+  })
+}
 
-//     // Create table
-//     db.run(CREATE_TABLE, function(err) {
-//       if (err) {
-//         console.log(err)
-//       } else {
-//         console.log('CREATE TABLE')
-//       }
-//     })
-//   })
-// }
+// SEED_DATA
+let seedData = () => {
 
-// // SEED_DATA
-// let seedData = () => {
+  // Run SQL one at a time
+  db.serialize(function() {
 
-//   // Run SQL one at a time
-//   db.serialize(function() {
+    // Insert data into datatable
+    db.run(SEED_DATA, function(err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('INSERT DATA')
+      }
+    })
+  })
+}
 
-//     // Insert data into datatable
-//     db.run(SEED_DATA, function(err) {
-//       if (err) {
-//         console.log(err)
-//       } else {
-//         console.log('INSERT DATA')
-//       }
-//     })
-//   })
-// }
-
-// createTable()
-// seedData()
+createTable()
+seedData()
