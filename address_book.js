bee1d4ac0cab52543a5ca91f25b5aa9da31013e3
Ahmad Ahmadi & Ivan Gerard <pair+ahmadahmadi14.ivangerard@hacktiv8.com>
2016-09-13 20:59:11 +0700

    kec. Release 3

--- address_book.js
+++ address_book.js
@@ -1,21 +1,14 @@
 
 const repl = require ('repl');
 const sqlite = require ('sqlite3').verbose();
+const schema = require ('validate');
 
 let file = 'address_book.db';
 let db = new sqlite.Database(file);
 var fs = require('fs')
 var contactsJSON = JSON.parse(fs.readFileSync('contacts.json'))
-
-
-let insertJSON = () => {
-
-
-
-}
-
-insertJSON();
-
+var groupsJSON = JSON.parse(fs.readFileSync('groups.json'))
+var header_groupJSON = JSON.parse(fs.readFileSync('header_group.json'))
 
 class Contact {
   constructor(component) {
@@ -25,6 +18,8 @@ class Contact {
     this.email = component['email'],
     this.phone = component['phone']
   }
+
+
 //tambah kontak
   static add(){
     for (let i =0;i<contactsJSON.length;i++){
@@ -40,6 +35,7 @@ class Contact {
 
   }
 
+
   static del(first_name){
     for (let i =0;i<contactsJSON.length;i++){
       db.run (`DELETE FROM address_book where first_name='${first_name}'`, function(err){
@@ -52,7 +48,91 @@ class Contact {
 
     }
   }
+
+
+  static update(phone,first_name){
+
+      db.run (`UPDATE address_book set  phone = '${phone}' where first_name = '${first_name}'`, function(err){
+        if (err){
+          console.log(err);
+        } else {
+          console.log('UPDATE data');
+        }
+      })
+
+
+  }
+
 }
+
+
+class Header_Group {
+  constructor(component) {
+    this.header_group_id = component['header_group_id'],
+    this.group_id = component['group_id'],
+    this.user_id = component['user_id']
+  }
+
+  static add(){
+    for (let i =0;i<header_groupJSON.length;i++){
+
+      let query = `INSERT INTO header_group (group_id, user_id) VALUES ('${header_groupJSON[i].group_id}','${header_groupJSON[i].user_id}')`
+
+
+
+      db.run (query, function(err){
+        if (err){
+          console.log(err);
+        } else {
+          console.log('add Header group data');
+        }
+      })
+    }
+  }
+}
+
+
+
+class Groups {
+  constructor(component) {
+    this.group_id = component['group_id'],
+    this.group_name = component['group_name']
+  }
+
+  static add(){
+    for (let i =0;i<groupsJSON.length;i++){
+      db.run (`INSERT INTO groups ( group_name) VALUES ('${groupsJSON[i].group_name}')`, function(err){
+        if (err){
+          console.log(err);
+        } else {
+          console.log('add Groups  data');
+        }
+      })
+    }
+  }
+
+  static del(firstname){
+
+      db.run (`DELETE FROM address_book where first_name = '${firstname}' `, function(err){
+        if (err){
+          console.log(err);
+        } else {
+          console.log('del Groups  data');
+        }
+      })
+
+  }
+
+}
+
+let replServer = repl.start({prompt:">"})
+replServer.context.Header_Group = Header_Group
+replServer.context.Contact = Contact
+replServer.context.Groups = Groups
+// Header_Group.add();
+// Groups.add();
+
+// CLASS header_group
 
 
 //Contact.add();
@@ -61,15 +141,4 @@ class Contact {
 
 //Contact.del("Ivan");
 
-static update(first_name){
-  for (let i =0;i<contactsJSON.length;i++){
-    db.run (`UPDATE address_book set  first_name ="Ivan" where gender='pria'`, function(err){
-      if (err){
-        console.log(err);
-      } else {
-        console.log('UPDATE data');
-      }
-    })
-
-  }
-}
+//select ab.first_name, g.group_name,hg.header_group_id from address_book ab, groups g, header_group hg where hg.group_id = g.group_id and hg.user_id = ab.user_id;
